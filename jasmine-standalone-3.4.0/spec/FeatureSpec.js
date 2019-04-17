082477bd503ce5224ee3b455e3487f98f07eaed9
Phil <phil.board90@googlemail.com>
2019-04-17 13:05:09 +0100

    all tests pass. takeoff, land, stormy weather

--- jasmine-standalone-3.4.0/spec/FeatureSpec.js
+++ jasmine-standalone-3.4.0/spec/FeatureSpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Feature Test:', function(){
-  
+
   var plane;
   var airport;
 
@@ -12,6 +12,19 @@ describe('Feature Test:', function(){
 
   it('planes can be instructed to land at an airport', function(){
     plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport)
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
+  it('blocks takeoff when weather is stormy', function(){
+    plane.land(airport)
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
     expect(airport.planes()).toContain(plane);
   });
 });
--- jasmine-standalone-3.4.0/spec/PlaneSpec.js
+++ jasmine-standalone-3.4.0/spec/PlaneSpec.js
@@ -4,14 +4,20 @@ describe('Plane',function(){
 
   var plane;
   var airport;
-  
+
   beforeEach(function(){
     plane = new Plane();
-    airport = jasmine.createSpyObj('airport',['clearForLanding']); //spys are doubles. creates airport that can accept method clearForLanding
+    airport = jasmine.createSpyObj('airport',['clearForLanding', 'clearForTakeOff' ]); //spys are doubles. creates airport that can accept method clearForLanding
   });
 
   it('can land at an airport', function(){
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
+
+  it('can takeoff from an airport', function(){
+     plane.land(airport);
+     plane.takeoff();
+     expect(airport.clearForTakeOff).toHaveBeenCalled();
+   });
 });
